fb886e4db4c00a06abcf116f9c10c47b7bb5c8d7
kalmux1 <kal.mux.cyber@gmail.com>
2023-12-17 23:22:09 +0530

    Tool

--- start.sh
+++ start.sh
@@ -2,7 +2,14 @@
 
 
 #Installing The Neofetch Tool On The Local System 
-sudo apt-get install neofetch -y
+echo "Created by KALMUX "
+echo "Installing required packages for your system"
+echo "This may take some time based on your Internet speed"
+
+
+a=$(sudo apt-get install neofetch -y)
+echo "$a"
+
 
 # Running Neofetch For Grabbing The System deep information
 neofetch
