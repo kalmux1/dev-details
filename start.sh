6a1267d48e7c34ca66f2d512fbe24e4cd1e30f06
kalmux1 <kal.mux.cyber@gmail.com>
2023-12-17 23:35:18 +0530

    Tool

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
-
+echo " "
+echo " "
 #Installing The Neofetch Tool On The Local System 
-echo "                          DeV-DEtails                         "
+echo "                          DeV-DEtails                     v1.0"
 echo "                      Created by KALMUX                       "
 echo "          Installing required packages for your system        "
 echo "      This may take some time based on your Internet speed    "
@@ -15,3 +15,12 @@ a=$(sudo apt-get install neofetch -y)
 
 # Running Neofetch For Grabbing The System deep information
 neofetch
+
+
+#exiting 
+
+echo " "
+echo " "
+echo "Visit to my github for more hacking tools https://github.com/kalmux1"
+echo "                  Thanks for using this tool                        "
+echo "                         Hack daily                                 "
